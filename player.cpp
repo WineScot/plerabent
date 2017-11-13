7e5dbcb317596d21a02286527b20133bed4a4878
xBorox1 <M.Borowski282@gmail.com>
2017-11-13 08:34:53 +0100

    First player definition

---
+++ player.cpp
@@ -0,0 +1,49 @@
+#include "player.h"
+#include <cstdlib>
+
+Player::player(int base_hp, int base_attack, int base_defence)
+{
+    this->base_hp=base_hp;
+    hp=base_hp;
+    this->base_attack=base_attack;
+    attack=base_attack;
+    this->base_defence=base_defence;
+    defence=base_defence;
+    position=1;
+}
+
+Player::battle(Monster enemy)
+{
+    bool tour=rand()%2; //losowanie tury
+    while(hp>0 && enemy.hp>0)
+    {
+        int base=rand()%6+1; //rzut kostk¹
+        if(tour)
+        {
+            base+=attack-enemy.defence;
+            base=min(base,1);
+            enemy.hp-=base;
+        }
+        else
+        {
+            base+=enemy.attack-defence;
+            base=min(base,1);
+            hp-=base;
+        }
+        tour=!tour;
+    }
+}
+
+Player::c_event(Event e)
+{
+    battle(e.m); //walka z potworem
+    hp+=e.hp;
+    attack+=e.attack;
+    defence+=e.defence;
+}
+
+Player::p_move()
+{
+    int mov=rand()%6+1;
+    position+=mov; //tymczasowe
+}
--- player.h
+++ player.h
@@ -1,18 +1,21 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#inlude "event.h"
+#include "monster.h"
 
 class Player
 {
     public:
-        player();
+        player(int, int, int);
         virtual ~player();
         void p_move();
-        void c_event();
+        void battle(Monster);
+        void c_event(Event);
 
     private:
-        int hp,attack,defence;
-
-
+        int base_hp, base_attack, base_defence; //Bazowe statystyki
+        int hp, attack, defence; //statystyki
+        int position; //Pozycja gracza
 };
 
 #endif // PLAYER_H
