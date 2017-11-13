e02bd47aaf77ebd62c0dde6e0d5134c88e6abfda
Jaskier234 <jaskier234@gmail.com>
2017-11-13 08:41:29 +0100

    wczytywanie z piku v1

--- event.h
+++ event.h
@@ -1,17 +1,12 @@
 #include "monster.h"
 
-struct ev
-{
-    string name, plot;
-};
-
 class Event
 {
 private:
-    static ev tab[100];
-    string name, plot;
+    static string tab[100];
+    string plot;
     Monster m;
     int hp, attack, defence; //bonusy
 public:
-    init();
+    friend void init();
 };
