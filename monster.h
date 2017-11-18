274d15f35756de46806015df3e9a7d8fbf7523fd
jaskier234 <jaskier234@gmail.com>
2017-11-18 20:06:14 +0100

    monster.h

--- monster.h
+++ monster.h
@@ -1,14 +1,18 @@
 #ifndef MONSTER_H
 #define MONSTER_H
 
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
 class Monster
 {
-private:
-    Monster();
-    virtual ~Monster();
     int hp, attack, defence;
     string name;
 public:
+    Monster();
+    //virtual ~Monster();
     friend void init();
 };
 
