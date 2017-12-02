58d7f3016549dc3e07ebe3f7c3897ddebb1171b0
Jaskier234 <jaskier234@gmail.com>
2017-12-02 17:33:19 +0100

    Update thief.h

--- thief.h
+++ thief.h
@@ -6,8 +6,6 @@
 
 class Thief : public Player
 {
-    private:
-        int dodge; //umiejetnosc do omijania pulapek, nie wiem jak ma dzialac, poniewaz nie mamy pulapek ;)
     public:
         Thief(Plansza*);
 };
