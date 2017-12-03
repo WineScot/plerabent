a2a02ca1e83140d7ca4cc700fd867cb7389306a5
xBorox1 <M.Borowski282@gmail.com>
2017-12-03 23:09:36 +0100

    Update event.cpp

--- event.cpp
+++ event.cpp
@@ -16,9 +16,9 @@ Event::Event(int x, int y, int ind)
     int id=rand()%Num_event;
     plot=Event_plots[id];
     m=Monster();
-    hp=(rand()%8)+1;
-    attack=(rand()%8)+1;
-    defence=(rand()%8)+1;
+    hp=(rand()%10)+1;
+    attack=(rand()%10)+1;
+    defence=(rand()%10)+1;
 }
 
 void Event::ustaw_potwora(Monster en)
@@ -61,8 +61,8 @@ int Event::Y()
 
 void Event::wyswietl()
 {
-    std::cout<<plot.c_str()<<std::endl;
-    std::cout<<"Przed tobą pojawia się: "<<m.getName().c_str()<<std::endl;
+    std::cout<<plot<<std::endl;
+    std::cout<<"Przed tobą pojawia się: "<<m.getName()<<std::endl;
     std::cout<<"Punkty życia: "<<m.getHp()<<std::endl;
     std::cout<<"Atak: "<<m.getAtt()<<std::endl;
     std::cout<<"Obrona: "<<m.getDef()<<std::endl;
