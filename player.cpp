cee95fc3e4aeaaa8e21d466b3de655107c909069
xBorox1 <M.Borowski282@gmail.com>
2017-12-03 23:20:33 +0100

    Boss

--- player.cpp
+++ player.cpp
@@ -12,6 +12,13 @@ Player::Player(Plansza* p)
     position=1;
     this->p=p;
 }
+
+void Player::ustawimie()
+{
+    std::cout<<"Podaj swoje imię!"<<std::endl;
+    std::cin>>name;
+}
+
 void Player::battle(Event* eve)
 {
     srand(hp);
@@ -23,7 +30,7 @@ void Player::battle(Event* eve)
     system(clear);
     if(tour) std::cout<<"Atakujesz jako pierwszy!"<<std::endl;
     else std::cout<<"Przeciwnik atakuje jako pierwszy!"<<std::endl;
-    sleep(2000);
+    sleep(1000);
     while(hp>0 && monsterHp>0)
     {
         int base=rand()%6+1; //rzut kostką
@@ -32,7 +39,6 @@ void Player::battle(Event* eve)
             int w_attack = attack;
             system(clear);
             showstats();
-            std::cout<<"\n";
             eve->getM()->showstats(monsterHp);
             std::cout<<"Twoja tura!"<<std::endl;
             sleep(500);
@@ -52,13 +58,12 @@ void Player::battle(Event* eve)
             monsterHp-=base;
             sleep(500);
             std::cout<<"Zadałeś "<<base<<" obrażeń"<<std::endl;
-            sleep(1500);
+            sleep(1000);
         }
         else
         {
             system(clear);
             showstats();
-            std::cout<<"\n";
             eve->getM()->showstats(monsterHp);
             std::cout<<"Tura przeciwnika!"<<std::endl;
             sleep(500);
@@ -68,18 +73,12 @@ void Player::battle(Event* eve)
             hp-=base;
             std::cout<<"Przeciwnik zadał "<<base<<" obrażeń"<<std::endl;
             c_damage+=base;
-            WAIT;
+            sleep(1000);
         }
         tour=!tour;
     }
     system(clear);
-    if(hp<=0)
-    {
-
-        std::cout<<"Zginąłeś!"<<std::endl;
-        koniec=true;
-        return;
-    }
+    if(hp<=0) std::cout<<"Zginąłeś!"<<std::endl;
     else
     {
         std::cout<<"Pokonałeś przeciwnika!"<<std::endl;
@@ -90,14 +89,14 @@ void Player::battle(Event* eve)
             std::cout<<"Umiejętność druida";
             for(int i=0;i<3;i++)
             {
-                sleep(1000);
+                Sleep(1000);
                 std::cout<<".";
             }
             std::cout<<"\n";
             int l_heal=rand()%100+1;
             if(l_heal<30)
             {
-                hp=hp+c_damage/2; //jest 30% szans na odnowienie 50% obrazen
+                hp=hp+c_damage/2; //jest 43% szans na odnowienie 50% obrazen
                 std::cout<<c_damage/2<<" zostało odnowione\n";
             }
             else
@@ -105,6 +104,97 @@ void Player::battle(Event* eve)
 
         }
     }
+}
+
+void showpoints(int a, int b)
+{
+    std::cout<<"'Jest "<<a<<":"<<b<<" dla mnie!'"<<std::endl;
+}
+
+void odpow(std::string &odp)
+{
+    int now=GetTickCount();
+    getline(std::cin,odp);
+    std::cout<<std::endl;
+    if(GetTickCount()-now>5000) odp="babla";
+}
+
+void Player::BossBattle()
+{
+    std::cout<<"Oto docierasz do końca swojej przygody! Przechodziłeś przez góry, lasy, szkoły, miasta, a teraz docierasz do miejsca ostatecznego... Oto przed tobą drzwi, a na nich napis: 46.";
+    std::cout<<std::endl;
+    std::cout<<"Przed tobą pojawia się: Sir Kamiński!!!"<<std::endl;
+    std::cout<<"Wkraczasz na środek sali, na sobie czujesz wzrok zdziwionych ludzi siedzących w ławkach. Jednak o wiele bardziej czujesz przeszywający cię wzrok Sir Kamińskiego."<<std::endl;
+    std::cout<<"'To może jakieś panie do tablicy?' - odzywa się Sir Kamiński - 'panie "<<name<<", zapraszam!'"<<std::endl;
+    WAIT;
+    system(clear);
+    int KP=0, PP=0; //punkty
+    std::string odp;
+
+    std::cout<<"'Na każde pytanie masz 5 sekund, jeśli odpowiesz dobrze, ale po czasie - punkt dla mnie.'"<<std::endl;
+    WAIT;
+    system(clear);
+
+    std::cin.ignore();
+
+    std::cout<<"'Jakie było pierwsze zwierzę w kosmosie?'"<<std::endl;
+    odpow(odp);
+    if(odp=="mucha" || odp=="muszka" || odp=="muszka owocowa") PP++;
+    else KP++;
+    showpoints(KP,PP);
+    WAIT;
+    system(clear);
+
+    std::cout<<"'Ile wynosi odległość Ziemi od Słońca (au)?'"<<std::endl;
+    odpow(odp);
+    if(odp=="1") PP++;
+    else KP++;
+    showpoints(KP,PP);
+    WAIT;
+    system(clear);
+
+    std::cout<<"'Bezpośrednia zmiana ciała stałego w gaz to?'"<<std::endl;
+    odpow(odp);
+    if(odp=="resublimacja") PP++;
+    else KP++;
+    showpoints(KP,PP);
+    WAIT;
+    system(clear);
+
+    std::cout<<"'Czy amper jest jednostką układu SI (tak/nie)?'"<<std::endl;
+    odpow(odp);
+    if(odp=="nie") PP++;
+    else KP++;
+    showpoints(KP,PP);
+    WAIT;
+    system(clear);
+
+    std::cout<<"'Podaj treść drugiego prawa Maxwella.'"<<std::endl;
+    odpow(odp);
+    if(1!=1) PP++;
+    else KP++;
+    showpoints(KP,PP);
+    WAIT;
+    system(clear);
+
+    std::cout<<"Śmiechów dochodzących z sali nie było końca."<<std::endl;
+    std::cout<<"'No dobra, najwyższa pora, aby to zakończyć...' - powiedział Sir Kamiński"<<std::endl;
+    WAIT;
+    system(clear);
+
+    Monster* Kamis = new Monster;
+    Kamis->boss(PP);
+    Event* bossbattle = new Event(0,0,0);
+    bossbattle->ustaw_potwora(*Kamis);
+    battle(bossbattle);
+    std::cout<<"'Gratuluję! Jeszcze nikomu nie udało się mnie pokonać. No ale nad fizyką musisz jeszcze popracować...' - powiedział Sir Kamiński"<<std::endl;
+    std::cout<<"Widzisz jak wyciąga z kieszeni cukierka. Rzuca go w twoją stronę. Łapiesz go."<<std::endl;
+    std::cout<<"'Masz na zachętę. Możesz sobie postawić kropkę. Ale ponieważ nie znałeś treści drugiego prawa Maxwella, to kołeczek będzie.'"<<std::endl;
+    WAIT;
+    system(clear);
+    std::cout<<"Tak oto kończy się twoja przygoda... "<<std::endl;
+    showstats();
+    koniec=true;
 }
 
 void Player::c_event(Event *e)
@@ -145,6 +235,7 @@ void Player::p_move()
 void Player::showstats()
 {
     std::cout<<"Twoje statystyki:"<<std::endl;
+    std::cout<<"Nazwa: "<<name<<std::endl;
     std::cout<<"Punkty życia: "<<hp<<std::endl;
     std::cout<<"Atak: "<<attack<<std::endl;
     std::cout<<"Obrona: "<<defence<<std::endl;
@@ -153,8 +244,6 @@ void Player::showstats()
 void Player::odejmijhp(int val)
 {
     hp-=val;
-    if(hp<=0)
-        koniec=true;
 }
 
 void Player::addattack(int val)
